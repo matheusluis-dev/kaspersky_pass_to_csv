83bc33ee76d669df3d53efa5c5d6b77e6f7e7710
matheusluis-dev <matheusluis.dev@proton.me>
2023-07-26 16:35:54 -0300

    TOC

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,10 @@ fn main() -> std::io::Result<()> {
     let file_path = "files\\mocked_passwords.txt".to_string();
     
     let contents = read_to_string(file_path)?;
-    let collection: Vec<&str> = contents.split("---").filter(|x| x.contains("Website")).collect();
+    let collection: Vec<&str> = contents
+        .split("---")
+        .filter(|x| x.contains("Website"))
+        .collect();
 
     let mut csv_file = File::create("files\\converted.csv")?;
     for password in collection {
